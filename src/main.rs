5f24a80baeb1025ca897b2dca05f23d636d22871
Tom Virgo <t.virgo@iwoca.co.uk>
2023-08-17 17:30:35 +0100

    clean up endpoints

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,6 @@ fn index() -> &'static str {
     "Welcome to FPL wrapped..."
 }
 
-#[get("/fpl_endpoints")]
-fn fpl_endpoints() -> String {
-    get_fpl_url(&FPLEndpoint::GameweekInfo { event_id: 5 })
-}
-
 #[get("/overview")]
 async fn overview(state: &State<AppState>) -> String {
     let latest_created_at: chrono::DateTime<chrono::Utc> =
@@ -61,20 +56,6 @@ async fn overview(state: &State<AppState>) -> String {
     return String::from("did not add players data");
 }
 
-#[get("/players")]
-async fn players(state: &State<AppState>) -> Json<Vec<PlayerFromDB>> {
-    let players = sqlx::query_as::<_, PlayerFromDB>("SELECT * from players;");
-    let result = players.fetch_all(&state.pool).await.unwrap();
-    Json(result)
-}
-
-#[get("/managers")]
-async fn managers(state: &State<AppState>) -> Json<Vec<ManagerDB>> {
-    let managers = sqlx::query_as::<_, ManagerDB>("SELECT * from managers;");
-    let result = managers.fetch_all(&state.pool).await.unwrap();
-    Json(result)
-}
-
 #[get("/count_managers")]
 async fn count_managers(state: &State<AppState>) -> Json<i64> {
     let count: i64 = sqlx::query_scalar("SELECT COUNT(*) FROM managers")
@@ -93,63 +74,16 @@ async fn player_timeseries(state: &State<AppState>, player_id: i32) -> Json<Vec<
     Json(result)
 }
 
-#[get("/managers/<player_name>")]
-async fn get_managers(state: &State<AppState>, player_name: String) -> Json<Vec<ManagerDB>> {
+#[get("/get_manager_by_name/<player_name>")]
+async fn get_manager_by_name(state: &State<AppState>, player_name: String) -> Json<Vec<ManagerDB>> {
     let managers = sqlx::query_as::<_, ManagerDB>(
         "SELECT *
     FROM managers
-    WHERE player_name ILIKE $1;",
+    WHERE LOWER(player_name) = $1;",
     )
-    .bind(format!("%{}%", &player_name));
+    .bind(player_name.to_lowercase());
     let result = managers.fetch_all(&state.pool).await.unwrap();
     Json(result)
-}
-
-#[get("/add_managers")]
-async fn add_managers(state: &State<AppState>) -> Json<Vec<i32>> {
-    let start_idx_result: Result<i32, sqlx::Error> = sqlx::query_scalar(
-        "SELECT start_idx
-        FROM add_manager_logs
-        ORDER BY created_at DESC
-        LIMIT 1;",
-    )
-    .fetch_one(&state.pool)
-    .await;
-    let start_idx = match start_idx_result {
-        Ok(latest) => latest + 100,
-        Err(_) => 1,
-    };
-    let all_manager_ids: Vec<i32> = (start_idx..start_idx + 100).collect();
-    let managers =
-        sqlx::query_as::<_, ManagerDB>("SELECT * FROM managers WHERE manager_id = ANY($1)")
-            .bind(&all_manager_ids)
-            .fetch_all(&state.pool)
-            .await
-            .unwrap();
-    let manager_ids: Vec<i32> = managers.iter().map(|x| x.manager_id).collect();
-    let ids_to_add = ids_difference(all_manager_ids, manager_ids);
-    for manager_id in ids_to_add.clone() {
-        let manager_summary = pull_manager(manager_id).await.unwrap();
-        sqlx::query(
-            "INSERT INTO managers (manager_id, player_name, entry_name) VALUES ($1, $2, $3)",
-        )
-        .bind(manager_summary.id)
-        .bind(format!(
-            "{} {}",
-            manager_summary.player_first_name, manager_summary.player_last_name
-        ))
-        .bind(&manager_summary.name)
-        .execute(&state.pool)
-        .await
-        .unwrap();
-    }
-
-    sqlx::query("INSERT INTO add_manager_logs (start_idx) VALUES ($1)")
-        .bind(start_idx)
-        .execute(&state.pool)
-        .await
-        .unwrap();
-    Json(ids_to_add)
 }
 
 #[shuttle_runtime::main]
@@ -159,15 +93,74 @@ async fn rocket(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_rocket::
         "/",
         routes![
             index,
-            fpl_endpoints,
             overview,
-            players,
             player_timeseries,
-            add_managers,
-            get_managers,
-            managers,
+            get_manager_by_name,
             count_managers
         ],
     );
     Ok(rocket.into())
 }
+
+// OLD CODE FOR REFERENCE
+
+// #[get("/players")]
+// async fn players(state: &State<AppState>) -> Json<Vec<PlayerFromDB>> {
+//     let players = sqlx::query_as::<_, PlayerFromDB>("SELECT * from players;");
+//     let result = players.fetch_all(&state.pool).await.unwrap();
+//     Json(result)
+// }
+
+// #[get("/managers")]
+// async fn managers(state: &State<AppState>) -> Json<Vec<ManagerDB>> {
+//     let managers = sqlx::query_as::<_, ManagerDB>("SELECT * from managers;");
+//     let result = managers.fetch_all(&state.pool).await.unwrap();
+//     Json(result)
+// }
+
+// #[get("/add_managers")]
+// async fn add_managers(state: &State<AppState>) -> Json<Vec<i32>> {
+//     let start_idx_result: Result<i32, sqlx::Error> = sqlx::query_scalar(
+//         "SELECT start_idx
+//         FROM add_manager_logs
+//         ORDER BY created_at DESC
+//         LIMIT 1;",
+//     )
+//     .fetch_one(&state.pool)
+//     .await;
+//     let start_idx = match start_idx_result {
+//         Ok(latest) => latest + 100,
+//         Err(_) => 1,
+//     };
+//     let all_manager_ids: Vec<i32> = (start_idx..start_idx + 100).collect();
+//     let managers =
+//         sqlx::query_as::<_, ManagerDB>("SELECT * FROM managers WHERE manager_id = ANY($1)")
+//             .bind(&all_manager_ids)
+//             .fetch_all(&state.pool)
+//             .await
+//             .unwrap();
+//     let manager_ids: Vec<i32> = managers.iter().map(|x| x.manager_id).collect();
+//     let ids_to_add = ids_difference(all_manager_ids, manager_ids);
+//     for manager_id in ids_to_add.clone() {
+//         let manager_summary = pull_manager(manager_id).await.unwrap();
+//         sqlx::query(
+//             "INSERT INTO managers (manager_id, player_name, entry_name) VALUES ($1, $2, $3)",
+//         )
+//         .bind(manager_summary.id)
+//         .bind(format!(
+//             "{} {}",
+//             manager_summary.player_first_name, manager_summary.player_last_name
+//         ))
+//         .bind(&manager_summary.name)
+//         .execute(&state.pool)
+//         .await
+//         .unwrap();
+//     }
+
+//     sqlx::query("INSERT INTO add_manager_logs (start_idx) VALUES ($1)")
+//         .bind(start_idx)
+//         .execute(&state.pool)
+//         .await
+//         .unwrap();
+//     Json(ids_to_add)
+// }
